aae1cb207cacbde71df3baa6d5d807da0c838f8e
jaKusmara <loste3r@gmail.com>
2024-10-08 17:04:36 +0200

    stairs

---
+++ stairs/stairs.c
@@ -0,0 +1,50 @@
+#include <karel.h>
+
+#define SPEED 100
+
+void turn_right();
+void move_on_stair();
+bool right_is_clear();
+
+int main() {
+    turn_on("stairs3.kw");
+
+    set_step_delay(SPEED);
+
+    step();
+
+    while(!front_is_clear()){
+    	move_on_stair();
+
+	while(beepers_present()){
+		pick_beeper();
+	}
+    }
+
+    while(beepers_in_bag()){
+    	put_beeper();
+    }
+
+    turn_off();
+
+    return 0;
+}
+
+void turn_right(){
+	turn_left();
+	turn_left();
+	turn_left();
+}
+
+void move_on_stair(){
+	turn_left();
+	step();
+	turn_right();
+
+	if(front_is_clear()){
+		step();
+	}else{
+		move_on_stair();
+	}
+	
+}
